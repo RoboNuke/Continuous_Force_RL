08c1ce639d0b9890208a6acb4cbd237b74486719
Hunter <brownhun@oregonstate.edu>
2025-04-22 00:29:06 -0700

    tested multi agent threading again

--- exp_control/sbatch_exp_launcher.bash
+++ exp_control/sbatch_exp_launcher.bash
@@ -7,6 +7,6 @@ nick_names=("PiH" "Gear_Mesh" "Nut_Thread" "Force_only_DMP" "Force_only_Hist")
 
 for env_idx in $(seq 0 $((num_exp_env - 1)))
 do
-    sbatch -J "${names[$env_idx]}_$1" -a 1-$num_exp_per_env exp_control/hpc_batch.bash $env_idx $num_agents "$1_${names[$env_idx]}"
+    sbatch -J "${names[$env_idx]}_$1" -a 1-$num_exp_per_env exp_control/hpc_batch.bash $env_idx $num_agents "$1_${nick_names[$env_idx]}"
     #sbatch -a 1-$num_exp_per_env exp_control/hpc_batch.bash $env_idx $num_agents "$1_$env_idx"
 done
