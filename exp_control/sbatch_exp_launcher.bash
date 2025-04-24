1cd73059f295799546a34681cb1b85b141a09799
Hunter <brownhun@oregonstate.edu>
2025-04-23 23:43:05 -0700

    Added kl adaptive learning rate

--- exp_control/sbatch_exp_launcher.bash
+++ exp_control/sbatch_exp_launcher.bash
@@ -2,10 +2,10 @@
 
 num_agents=4
 num_exp_per_env=2
-num_exp_env=3
+num_exp_env=1
 nick_names=("PiH" "Gear_Mesh" "Nut_Thread")
 
-for env_idx in $(seq 1 $((num_exp_env - 1)))
+for env_idx in $(seq 0 $((num_exp_env - 1)))
 do
     sbatch -J "${nick_names[$env_idx]}_$1" -a 1-$num_exp_per_env exp_control/hpc_batch.bash $env_idx $num_agents "$1_${nick_names[$env_idx]}"
     #sbatch -a 1-$num_exp_per_env exp_control/hpc_batch.bash $env_idx $num_agents "$1_$env_idx"
