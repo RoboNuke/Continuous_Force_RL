b6a1a9946984579f8cb54c2c60512992438a3a81
Hunter <brownhun@oregonstate.edu>
2025-04-29 12:50:36 -0700

    added debugging

--- exp_control/record_ckpts/get_next_ckpt.py
+++ exp_control/record_ckpts/get_next_ckpt.py
@@ -11,14 +11,17 @@ if __name__=="__main__":
     output = ""
     lock = FileLock(queue_fp + ".lock")
     with lock:
+        print("got lock")
         raw_args = None
         with open(queue_fp, "r+") as f:
             raw_args = f.readline().strip().split()
             data = f.read()
             f.truncate(0)
             f.write(data)
+        print("got new data")
         output = f"{raw_args[0]} {raw_args[1]} {raw_args[2]}"
         with open(tmp_file, 'w') as f:
             f.write(output)
+        print("wrote new data")
     sys.exit(0)
             
