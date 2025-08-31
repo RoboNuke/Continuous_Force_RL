Fixed gradients in block, improved serial trainer slightly

agents/MultiWandbLoggerPPO.py
@@ -519,15 +519,15 @@ class MultiWandbLoggerPPO(WandbLoggerPPO):
                 try:
                     models_cfg = {k: v._modules for (k, v) in self.models.items()}
                 except AttributeError:
-                    print("[INFO]: Extracting model config had Attribute Error...")
+                    #print("[INFO]: Extracting model config had Attribute Error...")
                     models_cfg = {k: v[i]._modules for (k, v) in self.models.items()}
-                    print("[INFO]: Attribute Error Handled")
+                    #print("[INFO]: Attribute Error Handled")
                 self.cfg['experiment'] = self.agent_exp_cfgs[i]['experiment']
                 self.cfg['break_force'] = break_forces[ i // 5 ] #TODO NO MAGIC NUMBERS!
                 wandb_config={**self.cfg, **trainer_cfg, **models_cfg, "num_envs":self.num_envs // self.num_agents}
                 # set default values
                 exp_dir = os.path.join(self.cfg['experiment']['directory'], self.cfg['experiment']['experiment_name'])
-                print("logger exp dir:", exp_dir)
+                #print("logger exp dir:", exp_dir)
                 wandb_kwargs = copy.deepcopy(self.agent_exp_cfgs[i].get("experiment", {}).get("wandb_kwargs", {}))
                 wandb_kwargs.setdefault("name", exp_dir)
                 wandb_kwargs.setdefault("config", {})
@@ -616,7 +616,7 @@ class MultiWandbLoggerPPO(WandbLoggerPPO):
 
             # time-limit (truncation) boostrapping
             if self._time_limit_bootstrap:
-                print("Time limit boostrapping")
+                #print("Time limit boostrapping")
                 rewards += self._discount_factor * values * truncated
             
             
@@ -785,8 +785,8 @@ class MultiWandbLoggerPPO(WandbLoggerPPO):
                 entropies[i,:,:],
                 policy_losses[i,:],
                 value_losses[i,:],
-                None, #policy_state,
-                None, #critic_state,
+                policy_state,
+                critic_state,
                 
                 self.optimizer
             )
@@ -909,6 +909,7 @@ class MultiWandbLoggerPPO(WandbLoggerPPO):
         # learning epochs
         for epoch in range(self._learning_epochs):
             mini_batch = 0
+            #print(f"Epoch:{epoch}")
             # mini-batches loop
             for (
                 sampled_states,
@@ -918,7 +919,7 @@ class MultiWandbLoggerPPO(WandbLoggerPPO):
                 sampled_returns,
                 sampled_advantages,
             ) in sampled_batches:
-
+                #print(f"Mini Batch:{mini_batch}")
                 sample_size = sampled_states.size()[0]
                 keep_mask = torch.ones((self.num_agents,), dtype=bool, device=self.device)
                 with torch.autocast(device_type=self._device_type, enabled=self._mixed_precision):
@@ -1015,6 +1016,7 @@ class MultiWandbLoggerPPO(WandbLoggerPPO):
                     policy_losses,
                     value_losses
                 )
+                mini_batch += 1
 
 
             self._one_time_metrics(

envs/factory/factory_env.py
@@ -546,7 +546,7 @@ class FactoryEnv(DirectRLEnv):
         self._compute_intermediate_values(dt=self.physics_dt)
         #time_out = self.episode_length_buf >= self.max_episode_length #- 1
         if self.common_step_counter % (self.max_episode_length) == 0:
-            print("Common Step:", self.common_step_counter)
+            #print("Common Step:", self.common_step_counter)
             time_out = torch.ones_like(self.ep_succeeded)
         else:
             time_out = torch.zeros_like(self.ep_succeeded)

learning/launch_utils.py
@@ -450,7 +450,7 @@ def set_models(env_cfg, agent_cfg, args_cli, env):
             sigma_idx = 6 if args_cli.control_torques else 3
         else:
             sigma_idx = 0
-        models['policy'] = SimBaActor( #BroAgent(
+        models['policy'] = torch.compile(SimBaActor( #BroAgent(
             observation_space=env.cfg.observation_space, 
             action_space=env.action_space,
             #action_gain=0.05,
@@ -461,16 +461,16 @@ def set_models(env_cfg, agent_cfg, args_cli, env):
             sigma_idx = sigma_idx,
             num_agents = env_cfg.num_agents, #args_cli.num_agents,
             last_layer_scale=agent_cfg['models']['last_layer_scale']
-        ) 
+        ) )
 
-    models["value"] = SimBaCritic( #BroAgent(
+    models["value"] = torch.compile(SimBaCritic( #BroAgent(
         state_space_size=env.cfg.state_space, 
         device=env.device,
         critic_output_init_mean = agent_cfg['models']['critic_output_init_mean'],
         critic_n = agent_cfg['models']['critic']['n'],
         critic_latent = agent_cfg['models']['critic']['latent_size'],
         num_agents = env_cfg.num_agents #args_cli.num_agents
-    )
+    ))
     print("[INFO]: Models created")
     return models
 
@@ -523,14 +523,35 @@ def set_block_agent(env_cfg, agent_cfg, args_cli, models, memory, env=None):
 
     agent.optimizer = make_agent_optimizer(
         models['policy'],
-        models['value'].critic,
+        models['value'],
         policy_lr = agent_cfg['agent']['learning_rate'],
         critic_lr = agent_cfg['agent']['learning_rate'],
         betas=(0.999, 0.999),
         eps=1e-8,
-        weight_decay=0
+        weight_decay=0,
+        debug=args_cli.debug_mode
     )
-    
+    """
+    print("Named parameters in the optimizer:")
+    for i, param_group in enumerate(agent.optimizer.param_groups):
+        print(f"\nParameter Group {i+1}:")
+        for j, param in enumerate(param_group['params']):
+            # Find the name of the parameter by iterating through model.named_parameters()
+            # This is necessary because the optimizer only stores the parameter tensors, not their names
+            param_name = None
+            for k, model in enumerate(models):
+                for name, model_param in models[model].named_parameters():
+                    if model_param is param:
+                        param_name = name
+                        break
+                    
+                if param_name:
+                    print(f"    Parameter {j+1} (Name: {param_name}, Shape: {param.shape})")
+                else:
+                    print(f"    Parameter {j+1} (Shape: {param.shape})")
+    #print(agent.optimizer.param_groups)
+    assert 1==0
+    """
     print("[INFO]: Block Agents and optimizer generated")
     return agent
     
@@ -572,10 +593,54 @@ def set_agent(env_cfg, agent_cfg, args_cli, models, memory, env=None):
         lr=agent_cfg['agent']['learning_rate'],
         betas=(0.999, 0.999)
     )
-    
+
+    #for name, param in agent.optimizer.named_parameters():
+    #    print(f"Name: {name}, Shape: {param.size()}")
+    """
+    print("Named parameters in the optimizer:")
+    for i, param_group in enumerate(agent.optimizer.param_groups):
+        print(f"\nParameter Group {i+1}:")
+        for j, param in enumerate(param_group['params']):
+            # Find the name of the parameter by iterating through model.named_parameters()
+            # This is necessary because the optimizer only stores the parameter tensors, not their names
+            param_name = None
+            for model in models:
+                for name, model_param in models[model].named_parameters():
+                    if model_param is param:
+                        param_name = name
+                        break
+                    
+                if param_name:
+                    print(f"    Parameter {j+1} (Name: {param_name}, Shape: {param.shape})")
+                else:
+                    print(f"    Parameter {j+1} (Shape: {param.shape})")
+    #print(agent.optimizer.param_groups)
+    assert 1==0
+    """
     print("[INFO]: Agent(s) and optimizer generated")
     return agent
-    
+
+def print_warn(warning):
+    print('\033[1;33;40m [WARN]: {warning} \033[0m')
+
+def attach_grad_debug_hooks(model, role='policy'):
+    """
+    Internal helper: attach gradient debug hooks to parameters.
+    Prints warning if grad is None or zero.
+    """
+    for pname, p in model.named_parameters():
+        if not p.requires_grad:
+            continue
+        #pname = getattr(p, "_name", "unnamed")
+        def make_hook(pname=pname, role=role):
+            def hook(grad):
+                if grad is None:
+                    print_warn("f{role} {pname} → NO GRAD")
+                elif grad.norm().item() == 0:
+                    print_warn(f"{role} {pname} → ZERO GRAD")
+                return grad
+            return hook
+        p.register_hook(make_hook())
 
 def check_logprob_consistant_loop(trainer, vid_env):
     import copy

models/block_simba.py
@@ -14,8 +14,8 @@ from models.feature_net import NatureCNN, layer_init, he_layer_init
 class BlockLinear(nn.Module):
     def __init__(self, num_blocks, in_features, out_features, name="linear"):
         super().__init__()
-        self.weight = nn.Parameter(torch.zeros(num_blocks, out_features, in_features))
-        self.bias = nn.Parameter(torch.zeros(num_blocks, out_features))
+        self.weight = nn.Parameter(torch.zeros(num_blocks, out_features, in_features), requires_grad=True)
+        self.bias = nn.Parameter(torch.zeros(num_blocks, out_features), requires_grad=True)
         # init each block separately
         #print(name, in_features, out_features)
         for i in range(num_blocks):
@@ -44,8 +44,8 @@ class BlockLayerNorm(nn.Module):
         self.affine = affine
         shape = (num_blocks, normalized_shape)
         if affine:
-            self.weight = nn.Parameter(torch.ones(*shape))
-            self.bias = nn.Parameter(torch.zeros(*shape))
+            self.weight = nn.Parameter(torch.ones(*shape), requires_grad=True)
+            self.bias = nn.Parameter(torch.zeros(*shape), requires_grad=True)
             for i in range(num_blocks):
                 self.weight[i]._agent_id = i
                 self.bias[i]._agent_id = i
@@ -152,7 +152,7 @@ class BlockSimBaActor(GaussianMixin, Model):
         self.actor_logstd = nn.ParameterList(
             [
                 nn.Parameter(
-                    torch.ones(1, self.num_actions) * math.log(act_init_std)
+                    torch.ones(1, self.num_actions) * math.log(act_init_std), requires_grad=True
                 )
                 for _ in range(num_agents)
             ]
@@ -339,36 +339,78 @@ def pack_agents_into_block(block_model: nn.Module, agent_models: dict):
 
 import torch.optim as optim
 
-
 def make_agent_optimizer(
-        policy_block,
-        # policy_std,
-        critic_block,
+        block_policy,
+        block_critic,
         policy_lr,
         critic_lr,
         betas=(0.9, 0.999),
         eps=1e-8,
-        weight_decay=0
+        weight_decay=0,
+        debug=False
 ):
     """
     Create a single Adam optimizer with parameter groups for each agent's
     policy and critic, with different base learning rates.
     """
+    param_groups = [
+        {"params": list(block_policy.parameters()), "lr": policy_lr, "role": "policy"},
+        {"params": list(block_critic.parameters()), "lr": critic_lr, "role": "critic"},
+    ]
+    if debug:
+        from learning.launch_utils import attach_grad_debug_hooks
+        attach_grad_debug_hooks(block_policy, "policy")
+        attach_grad_debug_hooks(block_critic, "critic")
+        
+    return torch.optim.Adam(param_groups, betas=betas, eps=eps, weight_decay=weight_decay)
+
+"""
     num_agents = policy_block.num_agents
     assert critic_block.num_agents == num_agents, "Policy and critic must have same number of agents"
-
+    
     param_groups = []
 
     for i in range(num_agents):
         # Policy group
-        policy_params = [p for p in policy_block.parameters() if hasattr(p, "_agent_id") and p._agent_id == i]
+        policy_params = [
+            p for p in policy_block.parameters()
+            if hasattr(p, "_agent_id") and p._agent_id == i
+        ]
+        for name, p in policy_block.named_parameters():
+            print(name) #p._agent_id, p._name)
+
+        for p in policy_params:
+            print(p._name)
         #policy_params.append( policy_std[i] )
-        param_groups.append({"params": policy_params, "lr": policy_lr})
+        param_groups.append({
+            "params": policy_params,
+            "lr": policy_lr,
+            "agent_id": i,
+            "role": "policy"
+        })
+        assert len(policy_params) > 0, f"Policy Param {i} not added to param group"
+        print(f"[INFO]: Created policy param group for agent {policy_params[0]._agent_id}, with lr={policy_lr} and size={len(policy_params)}")
+        if debug:
+            attach_grad_debug_hooks(policy_params, i, "policy")
         
 
         # Critic group
-        critic_params = [p for p in critic_block.parameters() if hasattr(p, "_agent_id") and p._agent_id == i]
-        param_groups.append({"params": critic_params, "lr": critic_lr})
+        critic_params = [
+            p for p in critic_block.parameters()
+            if hasattr(p, "_agent_id") and p._agent_id == i
+        ]
+        param_groups.append({
+            "params": critic_params,
+            "lr": critic_lr,
+            "agent_id": i,
+            "role": "critic"
+        })
+        assert len(critic_params) > 0, f"Critic Param {i} not added to param group"
+        print(f"[INFO]: Created critic param group for agent {critic_params[0]._agent_id}, with lr={critic_lr} and size={len(critic_params)}")
+        if debug:
+            attach_grad_debug_hooks(critic_params, i, "critic")
 
     optimizer = torch.optim.Adam(param_groups, betas=betas, eps=eps, weight_decay=weight_decay)
     return optimizer
+    """
+    