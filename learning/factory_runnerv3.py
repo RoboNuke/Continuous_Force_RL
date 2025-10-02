a1058fd95f987616fe94df11eb3617c536143fd5
Hunter <brownhun@oregonstate.edu>
2025-10-02 00:02:52 -0700

    Turned off all the camera stuff

--- learning/factory_runnerv3.py
+++ learning/factory_runnerv3.py
@@ -22,8 +22,8 @@ AppLauncher.add_app_launcher_args(parser)
 # parse the arguments
 args_cli, hydra_args = parser.parse_known_args()
 
-args_cli.video = True #False
-args_cli.enable_cameras = True #False
+args_cli.video = False
+args_cli.enable_cameras = False
 args_cli.headless = True
 
 # clear out sys.argv for Hydra
