3c78d8d7058a47162fd5875a3d150fa25c0aaa89
Hunter <brownhun@oregonstate.edu>
2026-01-27 22:36:33 -0800

    Added more noise to hex training

--- launcher/hpc_wandb_eval_batch.bash
+++ launcher/hpc_wandb_eval_batch.bash
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH -J WANDB_EVAL_JOB		    # job name (will be overridden by sbatch -J)
 #SBATCH -A virl-grp	                # sponsored account name
-#SBATCH -p tiamat,gpu,eecs2         # partition names
-#SBATCH --time=0-11:59:59           # time limit: 4 hours
+#SBATCH -p tiamat,gpu,dgx2,dgxh,eecs2         # partition names
+#SBATCH --time=0-02:59:59           # time limit: 4 hours
 #SBATCH --gres=gpu:1                # number of GPUs to request
 #SBATCH --mem=32G                   # request 32 gigabytes memory
 #SBATCH -c 12                       # number of cores/threads per task
